0f9ce742c32a4bb65a18aafb1c845bef4f386e34
Harneet_shah_singh <harneet.shah@teamlbm.com>
2024-12-05 17:56:01 +0530

    cors middleware updated

--- src/app/middlewares/corsMiddleware.js
+++ src/app/middlewares/corsMiddleware.js
@@ -1,9 +1,15 @@
-let whitelist = ["https://www.google.com", "http://192.168.11.89:3000" ,"http://192.168.1.72:3000" /*Home*/ , "http://192.168.11.65:3000", "http://192.168.11.94:3001", "http://192.168.11.94:3000"];
+let whitelist = ["http://192.168.11.89:3000", "http://localhost:3001", "http://192.168.11.89:3001" ,"http://192.168.1.72:3000", "http://192.168.1.72:3001" ,"http://192.168.11.65:3000" , "http://192.168.11.65:3001", "http://192.168.11.81:3000" , "http://192.168.11.81:3001", "http://192.168.11.94:3001", "http://192.168.11.94:3000" , "http://localhost:3001"];
 let corsMiddleware = {
   origin: function (origin, callback) {
+    console.log({origin ,whitelist} );
+    console.log(whitelist.indexOf(origin) , whitelist.indexOf(origin) !== -1 ,  origin);
     if (whitelist.indexOf(origin) !== -1 || !origin) {
+
+      console.log({klkkl:"kllkl"})
       callback(null, true);
     } else {
+      console.log({klkkl:"fail"})
+
       callback(new Error("Not allowed by CORS"));
     }
   },
