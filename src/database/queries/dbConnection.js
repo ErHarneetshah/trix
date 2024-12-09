0ee171b790f1a1e6a98a206800479e5f34076dbc
Harneet_shah_singh <harneet.shah@teamlbm.com>
2024-12-09 14:53:05 +0530

    code udpated

--- src/database/queries/dbConnection.js
+++ src/database/queries/dbConnection.js
@@ -6,7 +6,7 @@ const dbConfig = new appConfig().getConfig();
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
   host: dbConfig.host,
   dialect: 'mysql',
-  logging: true,
+  logging: false,
   pool: {
     max: 20,
     min: 2,
