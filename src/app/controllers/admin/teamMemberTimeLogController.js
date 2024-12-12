ff1ddcf3eeac641930b1d62f9f1957f5f1ba3ebc
Harneet_shah_singh <harneet.shah@teamlbm.com>
2024-12-12 18:58:10 +0530

    code updated

--- src/app/controllers/admin/teamMemberTimeLogController.js
+++ src/app/controllers/admin/teamMemberTimeLogController.js
@@ -17,41 +17,21 @@ class teamMemberTimeLogController {
       let offset = (page - 1) * limit || 0;
       let logWhere = await helper.searchCondition(searchParam, searchable);
 
-
-      // Add date range filter to `reportWhere`
-      // if (startDate && endDate) {
-      //   logWhere.createdAt = { [Op.between]: [new Date(startDate), new Date(endDate)] };
-      // } else if (startDate) {
-      //   logWhere.createdAt = { [Op.gte]: new Date(startDate) };
-      // } else if (endDate) {
-      //   logWhere.createdAt = { [Op.lte]: new Date(endDate) };
-      // }
-
       if (date) {
         const startOfDay = new Date(date);
         startOfDay.setHours(0, 0, 0, 0);
-      
+
         const endOfDay = new Date(date);
         endOfDay.setHours(23, 59, 59, 999);
-      
+
         logWhere.createdAt = { [Op.between]: [startOfDay, endOfDay] };
       }
-
-      // if (startDate && endDate) {
-      //   logWhere.logged_in_time = { [Op.gte]: new Date(startDate) }; // Greater than or equal to startDate
-      //   logWhere.logged_out_time = { [Op.lte]: new Date(endDate) }; // Less than or equal to endDate
-      // } else if (startDate) {
-      //   logWhere.logged_in_time = { [Op.gte]: new Date(startDate) }; // Only filter by startDate
-      // } else if (endDate) {
-      //   logWhere.logged_out_time = { [Op.lte]: new Date(endDate) }; // Only filter by endDate
-      // }
 
       logWhere.company_id = req.user.company_id;
       const alldata = await TimeLog.findAndCountAll({
         where: logWhere, // Filters for `workReports`
         offset,
         limit,
-        // group: ["user_id"],
         attributes: ["id", "total_active_duration", "logged_in_time", "logged_out_time", "late_coming", "early_going"],
         include: [
           {
@@ -70,17 +50,6 @@ class teamMemberTimeLogController {
       });
       if (!alldata) return helper.failed(res, variables.NotFound, "No Data is available!");
 
-      
-      const workingEmpCount = await User.count({
-        where: {
-          // Your conditions go here, for example:
-          currentStatus: 1,
-          shift_id: 1, // Example condition
-        },
-      });
-
-      
-
       return helper.success(res, variables.Success, "All Data fetched Successfully!", alldata);
     } catch (error) {
       return helper.failed(res, variables.BadRequest, error.message);
@@ -90,39 +59,30 @@ class teamMemberTimeLogController {
   getTeamMemberLogFiltered = async (req, res) => {
     try {
       // Search Parameter filters and pagination code >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-      let { searchParam, limit, page,date,tab } = req.query;
+      let { searchParam, limit, page, date, tab } = req.query;
       let searchable = ["$user.fullname$"];
       limit = parseInt(limit) || 10;
       let offset = (page - 1) * limit || 0;
       let logWhere = await helper.searchCondition(searchParam, searchable);
 
-      // Add date range tab to `reportWhere`
-      // if (startDate && endDate) {
-      //   logWhere.createdAt = { [Op.between]: [new Date(startDate), new Date(endDate)] };
-      // } else if (startDate) {
-      //   logWhere.createdAt = { [Op.gte]: new Date(startDate) };
-      // } else if (endDate) {
-      //   logWhere.createdAt = { [Op.lte]: new Date(endDate) };
-      // }
+      let startOfDay = new Date();
+      startOfDay.setHours(0, 0, 0, 0);
+
+      let endOfDay = new Date();
+      endOfDay.setHours(23, 59, 59, 999);
+
+      logWhere.createdAt = { [Op.between]: [startOfDay, endOfDay] };
 
       if (date) {
-        const startOfDay = new Date(date);
+        startOfDay = new Date(date);
         startOfDay.setHours(0, 0, 0, 0);
-      
-        const endOfDay = new Date(date);
+
+        endOfDay = new Date(date);
         endOfDay.setHours(23, 59, 59, 999);
-      
+
         logWhere.createdAt = { [Op.between]: [startOfDay, endOfDay] };
       }
 
-      // if (startDate && endDate) {
-      //   logWhere.logged_in_time = { [Op.gte]: new Date(startDate) }; // Greater than or equal to startDate
-      //   logWhere.logged_out_time = { [Op.lte]: new Date(endDate) }; // Less than or equal to endDate
-      // } else if (startDate) {
-      //   logWhere.logged_in_time = { [Op.gte]: new Date(startDate) }; // Only tab by startDate
-      // } else if (endDate) {
-      //   logWhere.logged_out_time = { [Op.lte]: new Date(endDate) }; // Only tab by endDate
-      // }
       if (tab) {
         if (tab.toLowerCase() === "working") {
           userWhere.currentStatus = 1;
@@ -133,12 +93,11 @@ class teamMemberTimeLogController {
         }
       }
       logWhere.company_id = req.user.company_id;
-      
+
       const alldata = await TimeLog.findAndCountAll({
         where: logWhere, // Filters for `workReports`
         offset,
         limit,
-        // group: ["user_id"],
         attributes: ["id", "total_active_duration", "logged_in_time", "logged_out_time", "late_coming", "early_going"],
         include: [
           {
@@ -161,6 +120,29 @@ class teamMemberTimeLogController {
     } catch (error) {
       return helper.failed(res, variables.BadRequest, error.message);
     }
+  };
+
+  getFilterCount = async (req, res) => {
+    const alldata = await TimeLog.findAndCountAll({
+      where: logWhere, // Filters for `workReports`
+      offset,
+      limit,
+      attributes: ["id", "total_active_duration", "logged_in_time", "logged_out_time", "late_coming", "early_going"],
+      include: [
+        {
+          model: User,
+          as: "user",
+          required: true,
+          attributes: ["id", "fullname"],
+        },
+        {
+          model: shift,
+          as: "shift", // Alias for the associated `department` model
+          attributes: ["start_time", "end_time"], // Select specific fields
+        },
+      ],
+      order: [["createdAt", "DESC"]], // Sort by creation date (most recent first)
+    });
   };
 }
 
