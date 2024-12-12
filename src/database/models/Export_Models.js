8674ea016a712b765a82ff1524d542cec39375c1
Harneet shah singh <harneetshahs@gmail.com>
2024-12-13 00:49:25 +0530

    code pudate

--- src/database/models/Export_Models.js
+++ src/database/models/Export_Models.js
@@ -11,6 +11,20 @@ import blockedWebsites from "./blockedWebsitesModel.js";
 import blockedWebsites from './src/database/models/blockedWebsitesModel.js';
 import reportSettings from './src/database/models/reportSettingsModel.js';
 import { ProductiveApp }from "./ProductiveApp.js";
+import accessToken from "./accessTokenModel.js";
+import AppHistoryEntry from "./AppHistoryEntry.js";
+import { BlockedWebsites } from "./BlockedWebsite.js";
+import company from "./companyModel.js";
+import emailGateway from "./emailGatewayModel.js";
+import { ImageUpload } from "./ImageUpload.js";
+import module from "./moduleModel.js";
+import { Notification } from "./Notification.js";
+import rolePermission from "./rolePermissionModel.js";
+import ProductiveWebsite from "./ProductiveWebsite.js";
+import { System } from "./System.js";
+import TimeLog from "./TimeLog.js";
+
+
 
 const exportModels = [User, role, reportingManager, department, designation, shift, team, userSetting, blockedWebsites, ProductiveApp, reportSettings];
 
