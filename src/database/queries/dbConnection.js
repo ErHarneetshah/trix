0fee6fe40501b41e3ec47d04ce0f13a3dd167893
Harneet_shah_singh <harneet.shah@teamlbm.com>
2025-01-07 11:52:03 +0530

    code updated

--- src/database/queries/dbConnection.js
+++ src/database/queries/dbConnection.js
@@ -32,7 +32,7 @@ await ensureDatabaseExists();
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: 'mysql',
-    logging: true,
+    logging: false,
     timezone: '+05:30', // Asia/Kolkata timezone
     pool: {
         max: 20,
