2968984a9e012dcdb742f23600efd83937cdb6c9
Harneet_shah_singh <harneet.shah@teamlbm.com>
2025-01-07 12:56:49 +0530

    cod updated

--- src/app/controllers/admin/teamMemberController.js
+++ src/app/controllers/admin/teamMemberController.js
@@ -485,7 +485,7 @@ class teamMemberController {
       const textMessage = `Hello ${isUserExists.fullname},\n\nYour new password generated successfully!\n\nHere are your login details:\nEmail: ${isUserExists.email}\nPassword: ${plainTextPassword}\n\nPlease log in to the application with these credentials.\n\nBest regards`;
 
       const subject = "Emonitrix-Generate New Password";
-      const sendmail = await H.sendM(req.user.company_id, isUserExists.email, subject, textMessage);
+      const sendmail = await H.sendM(isUserExists.company_id, isUserExists.email, subject, textMessage);
 
       if (!sendmail.success) {
         return helper.failed(res, variables.BadRequest, "Failed to send Email");
