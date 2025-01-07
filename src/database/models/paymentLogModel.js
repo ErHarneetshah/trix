42f7383ae40f0f76b99c0d91735ba22465b93f6f
Harneet_shah_singh <harneet.shah@teamlbm.com>
2025-01-07 11:49:54 +0530

    payment module uncommented

--- src/database/models/paymentLogModel.js
+++ src/database/models/paymentLogModel.js
@@ -1,63 +1,63 @@
-// import { DataTypes } from "sequelize";
-// import sequelize from "../queries/dbConnection.js";
+import { DataTypes } from "sequelize";
+import sequelize from "../queries/dbConnection.js";
 
-// const paymentLog = sequelize.define(
-//   "payment_logs",
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//       allowNull: false,
-//     },
-//     company_id: {
-//       type: DataTypes.BIGINT,
-//       allowNull: false,
-//     },
-//     companyName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     companyEmail: {
-//       type: DataTypes.STRING,
-//       unique: true,
-//       allowNull: false,
-//     },
-//     planId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     planName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     amountPaid: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     allowedEmployeeCount: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     startDate: {
-//       type: DataTypes.DATEONLY,
-//       allowNull: false,
-//     },
-//     endDate: {
-//       type: DataTypes.DATEONLY,
-//       allowNull: false,
-//     },
-//     status: {
-//       type: DataTypes.BOOLEAN,
-//       allowNull: false,
-//       Comment: "0 For Pending, 1 For Active, 2 For Completed",
-//     }
-//   },
-//   {
-//     timestamps: true,
-//     underscored: false,
-//   }
-// );
+const paymentLog = sequelize.define(
+  "payment_logs",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    company_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    companyName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    companyEmail: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    planId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    planName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amountPaid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    allowedEmployeeCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    startDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      Comment: "0 For Pending, 1 For Active, 2 For Completed",
+    }
+  },
+  {
+    timestamps: true,
+    underscored: false,
+  }
+);
 
-// // await paymentLog.sync({alter:1});
-// export default paymentLog;
+// await paymentLog.sync({alter:1});
+export default paymentLog;
