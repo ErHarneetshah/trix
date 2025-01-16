8e6868d8a237223ee7278e3a96be76491bb8df6e
Harneet_shah_singh <harneet.shah@teamlbm.com>
2025-01-16 11:18:08 +0530

    Payment gateway code tested and env updated

--- src/app/config/appConfig.js
+++ src/app/config/appConfig.js
@@ -5,6 +5,9 @@ class appConfig {
   //private variables
   #config;
   #jwtConfig;
+  #imageBaseUrl;
+  #SuperAdminUrl;
+  #redirectUrl;
 
   constructor() {
     if (process.env.NODE_ENV == "local") {
@@ -17,6 +20,9 @@ class appConfig {
         app_id: process.env.APP_ID,
         app_secret: process.env.APP_SECRET,
       };
+      this.#imageBaseUrl = process.env.Local_Image_Url;
+      this.#SuperAdminUrl = process.env.SuperAdmin_Local_URL;
+      this.#redirectUrl = process.env.Redirect_Local_URL;
     } else if (process.env.NODE_ENV == "staging") {
       this.#config = {
         port: process.env.S_DB_PORT,
@@ -27,6 +33,9 @@ class appConfig {
         app_id: process.env.APP_ID,
         app_secret: process.env.APP_SECRET,
       };
+      this.#imageBaseUrl = process.env.Demo_Image_Url;
+      this.#SuperAdminUrl = process.env.SuperAdmin_Demo_URL;
+      this.#redirectUrl = process.env.Redirect_Demo_URL;
     } else {
       this.#config = {
         port: process.env.L_DB_PORT,
@@ -37,17 +46,29 @@ class appConfig {
         app_id: process.env.L_APP_ID,
         app_secret: process.env.L_APP_SECRET,
       };
+      this.#imageBaseUrl = process.env.Demo_Image_Url;
+      this.#SuperAdminUrl = process.env.SuperAdmin_Demo_URL;
+      this.#redirectUrl = process.env.Redirect_Demo_URL;
     }
     this.#config.collation = process.env.collation;
     this.#jwtConfig = process.env.JWT_SECRET_KEY;
   }
 
-  getConfig(){
+  getConfig() {
     return this.#config;
   }
 
-  getJwtConfig(){
+  getJwtConfig() {
     return this.#jwtConfig;
+  }
+  getImageUrl() {
+    return this.#imageBaseUrl;
+  }
+  getSuperAdminUrl() {
+    return this.#SuperAdminUrl;
+  }
+  getRedirectUrl() {
+    return this.#redirectUrl;
   }
 }
 
